7b0848deab58e1da246d81f984a1235b7afdcdc5
phanvu1706 <phanvu1706@gmail.com>
2024-03-28 09:48:40 +0700

    Reorganize project folder structure and fix bug.
    - Sắp xếp lại cấu trúc thư mục
    - Thi thoảng gặp bug cell sinh ra item mới nhưng vẫn còn item cũ tại vị trí đó, thử sửa code FillGap thành duyệt từ dưới lên thay vì duyệt từ trên xuống

---
+++ Assets/GameAssets/Scripts/Controllers/LevelMoves.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelMoves : LevelCondition
+{
+    private int m_moves;
+
+    private BoardController m_board;
+
+    public override void Setup(float value, Text txt, BoardController board)
+    {
+        base.Setup(value, txt);
+
+        m_moves = (int)value;
+
+        m_board = board;
+
+        m_board.OnMoveEvent += OnMove;
+
+        UpdateText();
+    }
+
+    public override void ResetCondition(float value)
+    {
+        base.ResetCondition(value);
+
+        m_moves = (int)value;
+
+        UpdateText();
+    }
+
+    private void OnMove()
+    {
+        if (m_conditionCompleted) return;
+
+        m_moves--;
+
+        UpdateText();
+
+        if (m_moves <= 0)
+        {
+            OnConditionComplete();
+        }
+    }
+
+    protected override void UpdateText()
+    {
+        m_txt.text = string.Format("MOVES:\n{0}", m_moves);
+    }
+
+    protected override void OnDestroy()
+    {
+        if (m_board != null) m_board.OnMoveEvent -= OnMove;
+
+        base.OnDestroy();
+    }
+}
